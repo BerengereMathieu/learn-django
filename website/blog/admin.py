cec3a2e0648d5937006a8a14e574fa22a10d9590
Berengere <berengere.ma@gmail.com>
2019-03-17 11:51:38 +0100

    Initialize administration interface

--- website/blog/admin.py
+++ website/blog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    How to display Post elements in administration interface
+    """
+
+    # displayed fields
+    list_display = ('title', 'slug', 'author', 'publish',
+                    'status')
+
+    # to filter results
+    list_filter = ('status', 'created', 'publish', 'author')
+
+    # searchable fields
+    search_fields = ('title', 'body')
+
+    # initialize slug with title
+    prepopulated_fields = {'slug': ('title',)}
+
+    # to display author with ga lookup widget
+    raw_id_fields = ('author',)
+
+    # to nagivate using date hierarchy
+    date_hierarchy = 'publish'
+
+    # post are ordered by author and publication date
+    ordering = ['status', 'publish']
+
+
+admin.site.register(Post, PostAdmin)
