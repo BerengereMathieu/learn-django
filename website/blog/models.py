6f0b40be67db47232b5dfd26b4f25cf48c19446c
Berengere <berengere.ma@gmail.com>
2019-03-17 15:30:37 +0100

    Create a view to list posts

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset() \
+            .filter(status='published')
 
 
 class Post(models.Model):
-
     # article status
     STATUS_CHOICES = (
-        ('brouillon', 'Brouillon'),
-        ('publié', 'Publié'),
+        ('draft', 'Draft'),
+        ('published', 'Published'),
     )
+
+    objects = models.Manager()  # The default manager.
+
+    published = PublishedManager()  # Our custom manager
 
     # title
     title = models.CharField(max_length=1000)
@@ -38,7 +48,16 @@ class Post(models.Model):
     # article status
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
-                              default=STATUS_CHOICES[0])
+                              default='draft')
+
+    def get_absolute_url(self):
+        """ build canonical url """
+
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
 
     class Meta:
         # articles are ordered in descending order
@@ -47,3 +66,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    published = PublishedManager()  # Our custom manager.
